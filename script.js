b29eec118b31b7aee97534f500501f138a41f6c7
Nicole <nicolefongjw@gmail.com>
2025-10-24 19:20:00 -0700

    slide-down nav bar + pages folder
    pages aren't linked yet so dont worry about them if they don't appear on screen
    
    included nav bar using slide-down animation. might change later depending on structure/design of website

--- script.js
+++ script.js
@@ -35,3 +35,14 @@ function initializeTheme() {
 
 // Run the initialization when the page loads
 initializeTheme();
+
+
+// nav bar animation (slide down, open and close)
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+  
